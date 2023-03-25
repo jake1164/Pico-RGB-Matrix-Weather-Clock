1f2d7bb5f9bf077c30d1727702c8e282dc78e608
Jason Jackson <jake1164@hotmail.com>
2023-03-25 08:25:09 -0400

    Added instructions and details to README

--- src/_boot.py
+++ src/_boot.py
@@ -1,7 +1,12 @@
 import board
 import digitalio
 import storage
-
+'''
+To enable persistant storage you need to rename this file to boot.py and place it on your system.
+Once renamed and placed on your system you will get a ReadOnly error when you try to copy files to the 
+device. You must hold the Menu button (KEY0) while turning on the device to make the device writable
+by your computer / usb device. 
+'''
 write_pin = digitalio.DigitalInOut(board.GP15)
 write_pin.direction = digitalio.Direction.INPUT
 write_pin.pull = digitalio.Pull.UP
